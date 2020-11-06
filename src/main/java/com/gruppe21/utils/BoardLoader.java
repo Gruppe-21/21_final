3083b565a02ac57480343a684d0836712124584c
Tobias Maneschijn <tfrom222@gmail.com>
2020-11-06 11:12:32 +0100

    refactoring variables

--- src/main/java/com/gruppe21/utils/BoardLoader.java
+++ src/main/java/com/gruppe21/utils/BoardLoader.java
@@ -19,8 +19,8 @@ import java.util.ArrayList;
 public class BoardLoader {
 
     public static ArrayList<Square> loadBoardFromFile(String fileName) throws ParserConfigurationException, IOException, SAXException {
-        Document doc = XMLUtil.getXMLDocument(fileName);
-        NodeList boardNodes = XMLUtil.getNodeListFromTag(doc, "board");
+        Document document = XMLUtil.getXMLDocument(fileName);
+        NodeList boardNodes = XMLUtil.getNodeListFromTag(document, "board");
         // NodeList cardNodes = getNodeListFromTag(doc, "cards");
 
         ArrayList<Square> squares = getSquaresFromNodeList(boardNodes);
@@ -32,17 +32,15 @@ public class BoardLoader {
         ArrayList<Square> squares = new ArrayList<Square>();
 
         for (int i = 0; i < boardNodes.getLength(); i++) {
-            Node nNode = boardNodes.item(i);
-
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                addXMLSquareToArrayList(squares, eElement);
+            Node node = boardNodes.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element tag = (Element) node;
+                addXMLSquareToArrayList(squares, tag);
             }
-
         }
+
         return squares;
     }
-
 
     //TODO SKAL ÆNDRES TIL TYPEN CHANCECARD NÅR DET ER IMPLEMENTERET
     private static ArrayList<String> getCardsFromNodeList(NodeList nodelist) {
@@ -50,18 +48,16 @@ public class BoardLoader {
 
         for (int i = 0; i < nodelist.getLength(); i++) {
             Node nNode = nodelist.item(i);
-
             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
-                // Add card to list
+                Element tag = (Element) nNode;
             }
 
         }
         return cards;
     }
 
-    private static void addXMLSquareToArrayList(ArrayList<Square> squares, Element eElement) {
-        String elementName = eElement.getNodeName();
+    private static void addXMLSquareToArrayList(ArrayList<Square> squares, Element tag) {
+        String elementName = tag.getNodeName();
         switch (elementName) {
             case "StartSquare":
                 // Add square
@@ -69,31 +65,24 @@ public class BoardLoader {
                 break;
             case "PropertySquare":
                 // Add square
-                String name = eElement.getAttribute("label");
+                String name = tag.getAttribute("label");
                 squares.add(new Square(name, "", 0, SquareType.Normal));
                 break;
             case "ChanceSquare":
                 // Add square
                 squares.add(new Square("Chance", "", 0, SquareType.Normal));
-
                 break;
             case "FreeParkingSquare":
                 // Add square
                 squares.add(new Square("Free parking", "", 0, SquareType.Normal));
-
-
                 break;
             case "GoToPrisonSquare":
                 // Add square
                 squares.add(new Square("Go to prison", "", 0, SquareType.Normal));
-
-
                 break;
             case "PrisonSquare":
                 // Add square
                 squares.add(new Square("Prison / Visit prison", "", 0, SquareType.Normal));
-
-
                 break;
             default:
                 break;
