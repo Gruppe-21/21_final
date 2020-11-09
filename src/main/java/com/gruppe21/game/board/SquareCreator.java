63deed7a29af78a7663399d136321f8a483a58cf
Troels <troelstoy@outlook.com>
2020-11-09 16:56:58 +0100

    Added square Added property constructor

--- src/main/java/com/gruppe21/game/board/SquareCreator.java
+++ src/main/java/com/gruppe21/game/board/SquareCreator.java
@@ -1,5 +1,34 @@
 package com.gruppe21.game.board;
 
+/*
+        Opretter en basic Square
+ */
+
 public class SquareCreator {
     private String name;
+
+    public SquareCreator(String name) {
+        this.name = name;
+    }
+}
+
+/*
+        Opretter en property square
+ */
+
+public class Property extends SquareCreator{
+    private int modifyvalue;
+    private boolean owned;
+    private boolean ownedPair;
+
+    public Property(String name, int modifyvalue, boolean owned, boolean ownedPair) {
+        super(name);
+        this.modifyvalue = modifyvalue;
+        this.owned = owned;
+        this.ownedPair = ownedPair;
+    }
+}
+
+public class Chance extends SquareCreator{
+    return 0; // Mangler at chance kortet er lavet
 }
