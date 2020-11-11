42ac9197a6877139f5dcf699c0e91be01c48f785
Gülsen <gulsencik@live.dk>
2020-11-11 16:07:24 +0100

    Updated ChanceCardMoney (Still unfinished)

--- src/main/java/com/gruppe21/game/board/chancecard/ChanceCardMoney.java
+++ src/main/java/com/gruppe21/game/board/chancecard/ChanceCardMoney.java
@@ -7,20 +7,53 @@ import java.util.List;
 
 public class ChanceCardMoney extends ChanceCard {
     private int money;
-    private List<Player> moneyOrigin; //Player-class is implemented later
+    private boolean isPayToBank;
+    private boolean isRecieveMoneyFromBank;
+    private boolean isRecieveMoneyFromStart;
 
-    public ChanceCardMoney(String description, int money) {
+
+    public ChanceCardMoney(String description, int money, boolean isPayToBank, boolean isRecieveMoneyFromBank, boolean isRecieveMoneyFromStart) {
         super(description);
         this.money = money;
+        this.isPayToBank = isPayToBank;
+        this.isRecieveMoneyFromBank = isRecieveMoneyFromBank;
+        this.isRecieveMoneyFromStart = isRecieveMoneyFromStart;
     }
 
     @Override
     public void use(Game game) {
-        super.use(game);
+
+        if(isPayToBank) {
+            tooMuchCandy(game);
+        } else if(isRecieveMoneyFromBank) {
+            finishedHomework(game);
+        } else if(isRecieveMoneyFromStart) {
+            startCard(game);
+        } else {
+            birthday(game);
+        }
+
+    }
+    //Den nuværende spiller mister 2M
+    private void tooMuchCandy(Game game) {
+//      int playerCurrentBankBalance = game.getPlayers()[playerIndex].getBankBalance;
+//      playerCurrentBankBalance -= 2;
+//      game.getPlayers()[playerIndex].getBankBalance.addBalance() = playerCurrentBankBalance;
+
     }
 
+    //Den nuværende spiller modtager 2M
+    private void finishedHomework(Game game) {
 
-    public void use(Player player, List<Player> moneyOrigin) {
+    }
+
+    //Den nuværende spiller rykkes til startfeltet og modtager derfor 2M
+    private void startCard(Game game) {
+
+    }
+
+    //Den nuværende spiller modtager 1M fra de andre spillere
+    private void birthday(Game game) {
 
     }
 
