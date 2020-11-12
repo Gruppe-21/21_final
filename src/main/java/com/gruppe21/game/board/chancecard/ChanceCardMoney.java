abc56f5a57702a91fe020156a49208220ec74e1d
s205436 <s205436@student.dtu.dk>
2020-11-12 15:29:49 +0100

    ChanceCardMoney.startcard() moved to own Class: ChanceCardStart

--- src/main/java/com/gruppe21/game/board/chancecard/ChanceCardMoney.java
+++ src/main/java/com/gruppe21/game/board/chancecard/ChanceCardMoney.java
@@ -1,7 +1,6 @@
 package com.gruppe21.game.board.chancecard;
 
 import com.gruppe21.game.Game;
-import com.gruppe21.game.board.Square;
 import com.gruppe21.player.BankBalance;
 import com.gruppe21.player.Player;
 
@@ -9,15 +8,13 @@ public class ChanceCardMoney extends ChanceCard {
     private int money;
     private boolean isPayToBank;
     private boolean isReceiveMoneyFromBank;
-    private boolean isMoveToStart;
     private boolean isBirthday;
 
-    public ChanceCardMoney(String description, int money, boolean isPayToBank, boolean isReceiveMoneyFromBank,boolean isMoveToStart, boolean isBirthday) {
+    public ChanceCardMoney(String description, int money, boolean isPayToBank, boolean isReceiveMoneyFromBank, boolean isBirthday) {
         super(description);
         this.money = money;
         this.isPayToBank = isPayToBank;
         this.isReceiveMoneyFromBank = isReceiveMoneyFromBank;
-        this.isMoveToStart = isMoveToStart;
         this.isBirthday = isBirthday;
     }
 
@@ -28,8 +25,6 @@ public class ChanceCardMoney extends ChanceCard {
             tooMuchCandy(player);
         } else if(isReceiveMoneyFromBank) {
             finishedHomework(player);
-        } else if(isMoveToStart){
-            startCard(game,player);
         }
     }
 
@@ -51,21 +46,6 @@ public class ChanceCardMoney extends ChanceCard {
     //Current player receives money
     private void finishedHomework(Player player) {
         int modifyBalance = money; // +2M
-        BankBalance playerCurrentBalance = player.getBankBalance();
-
-        playerCurrentBalance.addBalance(modifyBalance);
-    }
-
-    //Current player moves to StartSquare and receives money
-    private void startCard(Game game,Player player) {
-        int playerIndex = game.getCurrentPlayer();
-        int startSquareIndex = 1; // TO-DO: game.getBoard().getSquareAtNumber(start)
-        int modifyBalance = money; // +2M
-
-        game.getGuiWrapper().showMessage(description);
-        Square square = game.getBoard().getSquareAtNumber(startSquareIndex);
-        game.movePlayer(playerIndex, square);
-
         BankBalance playerCurrentBalance = player.getBankBalance();
 
         playerCurrentBalance.addBalance(modifyBalance);
