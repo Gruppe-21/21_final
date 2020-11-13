bf42452f811e76494afc88632c91de795fb7f670
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-11-13 09:33:20 +0100

    GAME: Whitespace changed

--- src/main/java/com/gruppe21/game/Game.java
+++ src/main/java/com/gruppe21/game/Game.java
@@ -220,6 +220,8 @@ public class Game {
         guiWrapper.updatePlayerBalance(playerindex, newBalance);
     }
 
+
+
     public void waitForUserAcknowledgement(String message) {
         if (isTest) return;
         guiWrapper.showMessage(message);
