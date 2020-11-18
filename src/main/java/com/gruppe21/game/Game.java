b29720f83ec74f813bf1b13491f8fc82c493c627
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-11-18 14:05:22 +0100

    Fixed teleportPlayer

--- src/main/java/com/gruppe21/game/Game.java
+++ src/main/java/com/gruppe21/game/Game.java
@@ -184,11 +184,10 @@ public class Game {
         guiManager.movePlayer(player, squareIndex);
         player.setCurrentSquareIndex(squareIndex);
         board.getSquareAtIndex(squareIndex).handleLandOn(player);
-
     }
 
     public void teleportPlayer(Player player, Square square){
-        teleportPlayer(players[playerIndex], board.getSquareIndex(square));
+        teleportPlayer(player, board.getSquareIndex(square));
     }
 
     private int getPlayerIndex(Player player){
