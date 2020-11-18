5ef18e1e5fae6a7c63a5bcc48e05a8cd911bb7fa
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-11-18 11:38:45 +0100

    Removed references in comments to toSekserer

--- src/main/java/com/gruppe21/player/Player.java
+++ src/main/java/com/gruppe21/player/Player.java
@@ -5,7 +5,6 @@ public class Player {
 
     private String name = "";            // The player's name
     private BankBalance bankBalance;        // The player's bank balance
-    //private boolean toSekserer = false;  // Checks whether or not the player played two 6
     private boolean nameEndsWithS;     // Checks whether or not the the player's name ends with an "s"
     private String[] piece;            // Piece color
     public Boolean prisonStatus;       // Boolean status whether Player is in prison or not
@@ -42,10 +41,6 @@ public class Player {
     public void setBankBalance(BankBalance bankBalance) {
         this.bankBalance = bankBalance;
     }
-
-    //public boolean isToSekserer() {return toSekserer;}
-
-    //public void setToSekserer(boolean toSekserer) {this.toSekserer = toSekserer;}
 
     // Gets the player's name as a string
     public String getName() {
