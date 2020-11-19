c68edd89b9a510c2401576cd9da5de728a7734cc
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-11-19 12:58:28 +0100

    GUIManager now has a sellProperties method. This should be moved.

--- src/main/java/com/gruppe21/gui/GUIManager.java
+++ src/main/java/com/gruppe21/gui/GUIManager.java
@@ -3,7 +3,10 @@ package com.gruppe21.gui;
 
 import com.gruppe21.game.Die;
 import com.gruppe21.game.board.Board;
+import com.gruppe21.game.board.squares.PropertySquare;
 import com.gruppe21.player.Player;
+import com.gruppe21.utils.arrayutils.OurArrayList;
+import com.gruppe21.utils.localisation.Localisation;
 
 import java.awt.*;
 
@@ -48,6 +51,44 @@ public class GUIManager {
     public void setGUIDice(Die[] dice) {
         if (isTest) return;
         guiWrapper.setDice(dice[0].getValue(), dice[1].getValue());
+    }
+
+    private static final int MAX_NUM_BUTTONS = 5;
+    public PropertySquare[] sellProperties(PropertySquare[] properties, String creditorName, int debt){
+        //Todo: should show next and previous buttons if there aren't any properties to show
+        OurArrayList<PropertySquare> selected = new OurArrayList<>();
+        Localisation localisation = Localisation.getInstance();
+        int currentlyViewing = 0;
+        int numPropertyButtons = Math.min(properties.length, MAX_NUM_BUTTONS - 2);
+        String[] buttons = new String[numPropertyButtons+2];
+        buttons[0] = localisation.getStringValue("nextBtn");
+        buttons[buttons.length-1] = localisation.getStringValue("previousBtn");
+        do {
+            for (int i = 0; i < numPropertyButtons-1; i++) {
+                buttons[i+1] = properties[currentlyViewing+i].getDescription();
+            }
+
+            String selectedButton = waitForUserButtonPress(localisation.getStringValue("sellProperties", Integer.toString(debt), creditorName), buttons);
+            if (selectedButton.equals(buttons[0]) && currentlyViewing < (properties.length - 3) ) currentlyViewing++;
+            else if(selectedButton.equals(buttons[buttons.length-1]) && currentlyViewing > 0) currentlyViewing--;
+            else {
+                for (PropertySquare property : properties) {
+                    if (property.getDescription().equals(selectedButton)){
+                        if (selected.indexOf(property) == -1){
+                            selected.add(property);
+                            debt -= property.getPrice();
+                        }
+                        else {
+                            selected.remove(property);
+                            debt += property.getPrice();
+                        }
+                    }
+                }
+
+            }
+
+        }while (debt > 0);
+        return selected.toArray(new PropertySquare[0]);
     }
 
     public void setGUIPlayerBalance(Player player, int newBalance) {
