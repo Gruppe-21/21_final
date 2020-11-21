225f4e27667590647ef90194132dd234889bdcd1
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2020-11-21 16:02:10 +0100

    GoToPrisonSquare has been refactored

--- src/main/java/com/gruppe21/game/board/squares/GoToPrisonSquare.java
+++ src/main/java/com/gruppe21/game/board/squares/GoToPrisonSquare.java
@@ -1,6 +1,7 @@
 package com.gruppe21.game.board.squares;
 
 import com.gruppe21.game.Game;
+import com.gruppe21.game.board.Board;
 import com.gruppe21.player.Player;
 
 public class GoToPrisonSquare extends Square {
@@ -11,22 +12,10 @@ public class GoToPrisonSquare extends Square {
 
     @Override
     public void handleLandOn(Player player, Game game) {
+        super.handleLandOn(player, game);
 
-        Square prisonSquare = null;
-
-        for (Square square : board.getSquares().toArray(new Square[0])) {
-            if(square.getClass() ==  GoToPrisonSquare.class){
-                prisonSquare = square;
-                break;
-            }
-        }
-
-        if(prisonSquare != null){
-            game.teleportPlayer(player, prisonSquare);
-            player.prisonStatus = true;
-
-        }
-
+        Board board = game.getBoard();
+        game.teleportPlayer(player, board.getSquareClosetToSquare(this, board.getSquaresOfClass(PrisonSquare.class)));
+        player.prisonStatus = true;
     }
-
 }
