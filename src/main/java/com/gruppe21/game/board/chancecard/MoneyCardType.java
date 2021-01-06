dc2edbfe0eeceaaf74b89b3af5c467e8a9934479
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-06 12:06:24 +0100

    Removed Board, ChanceCards, Squares, Deck and OurArrayList

--- src/main/java/com/gruppe21/game/board/chancecard/MoneyCardType.java
+++
@@ -1,6 +0,0 @@
-package com.gruppe21.game.board.chancecard;
-
-public enum MoneyCardType {
-    Birthday,
-    Bank
-}
--- src/main/java/com/gruppe21/game/board/chancecard/MoveCardType.java
+++
@@ -1,9 +0,0 @@
-package com.gruppe21.game.board.chancecard;
-
-public enum MoveCardType {
-    MoveToSquare,
-    MoveUpTo,
-    Figure,
-    TakeOrMove,
-    FreeSquare
-}
