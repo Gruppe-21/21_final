7d250bd023950f53a30b9dbcd4d342a2c77a1332
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-06 13:59:16 +0100

    Player: added START_FUNDS, balance, position, numSubscribers and onMovedSubscribers attributes. Added subscribeToOnMoved.

--- src/main/java/com/gruppe21/player/Player.java
+++ src/main/java/com/gruppe21/player/Player.java
@@ -8,9 +8,44 @@ import com.gruppe21.utils.arrayutils.OurArrayList;
 import com.gruppe21.utils.localisation.Localisation;
 import gui_fields.GUI_Player;
 
+import java.util.Objects;
+
 //Todo: add possesiveName
 
 public class Player {
+    private static final int START_FUNDS = 30000;
+
+    SquareController position;
+    int balance;
+    int numSubscribers = 0;
+    Object[] onMovedSubscribers = new Object[1]; //Shouldn't be Object
+
+    public void subscribeToOnMoved(Object subscriber){
+        //Lengthens array if too short
+        if (numSubscribers == onMovedSubscribers.length){
+            Object[] temp = new Object[onMovedSubscribers.length * 2];
+            for (int i = 0; i < onMovedSubscribers.length; i++) {
+                temp[i] = onMovedSubscribers[i];
+            }
+            onMovedSubscribers = temp;
+        }
+        onMovedSubscribers[numSubscribers] = subscriber;
+        numSubscribers++;
+    }
+
+    private void notifyMove(){
+        for (Object subscriber: onMovedSubscribers) {
+            //notify them!
+        }
+    }
+
+    public void moveTo(SquareController squareController){
+
+    }
+
+    public void teleportTo(SquareController squareController){
+
+    }
 
 }
 
