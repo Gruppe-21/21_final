19725011b47e7d576be2ce2b102f3f90193476db
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-06 12:07:25 +0100

    Emptied main in App

--- src/main/java/com/gruppe21/App.java
+++ src/main/java/com/gruppe21/App.java
@@ -3,10 +3,7 @@ import com.gruppe21.game.Game;
 
 public class App {
     public static void main(String[] args) {
-        // Start new game without predefined players
-        Game game = new Game();
-        //Game game = new Game(new Player[]{null, null});
-        game.startGame();
+
     }
 }
 
