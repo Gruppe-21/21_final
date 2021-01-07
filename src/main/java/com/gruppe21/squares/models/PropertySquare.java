a0c3299ac96b7cf890d1d83b303a90849135651e
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 12:23:39 +0100

    PropertySquare: set guiField to Gui_Street

--- src/main/java/com/gruppe21/squares/models/PropertySquare.java
+++ src/main/java/com/gruppe21/squares/models/PropertySquare.java
@@ -1,11 +1,16 @@
 package com.gruppe21.squares.models;
 
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Street;
+
 public class PropertySquare extends Square {
     private int houses;
 
     public PropertySquare(int statusEffect, int id) {
         super(statusEffect, id);
         houses = 0;
+        GUI_Street street = new GUI_Street();
+        setGuiField(street);
     }
 
     public int getHouses() {
