980b89c5b95784250d45a043062c7e8d9aba6e96
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-07 11:16:46 +0100

    PlayerController: drawCard now adds the drawn card its players Deck. drawCard now calls the cards onDraw method. Note that these function haven't been written yet.

--- src/main/java/com/gruppe21/player/PlayerController.java
+++ src/main/java/com/gruppe21/player/PlayerController.java
@@ -15,6 +15,8 @@ public class PlayerController {
 
     public CardController drawCard(Deck deck){
         CardController card = deck.nextCard();
+        player.getHeldCards().addCard(card);
+        card.onDraw(this);
         //Todo: probably call some function on the card. Also maybe add it to the players deck.
     }
 
