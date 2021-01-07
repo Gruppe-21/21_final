d7ed4f85c8d3784faa55c661101c2852326210bd
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-07 16:36:38 +0100

    GameView: added askForFirstPlayer and displayBoard

--- src/main/java/com/gruppe21/game/GameView.java
+++ src/main/java/com/gruppe21/game/GameView.java
@@ -1,6 +1,7 @@
 package com.gruppe21.game;
 
 import com.gruppe21.gui.GUIManager;
+import com.gruppe21.player.PlayerController;
 import com.gruppe21.utils.localisation.Localisation;
 
 public class GameView {
@@ -17,8 +18,24 @@ public class GameView {
         );
     }
 
+    public void displayBoard(Board board){
+        GUIManager.getInstance().displayBoard(board);
+    }
+
     public int askNumberOfPlayers(int minNumPlayers, int maxNumPlayers){
         return GUIManager.getInstance().getUserInteger("ASK NUM PLAYERS, GameView askNumberOfPlayers", minNumPlayers, maxNumPlayers);
+    }
+
+    public PlayerController askForFirstPlayer(PlayerController... playerControllers){
+        String[] names = new String[playerControllers.length];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = playerControllers[i].getName();
+        }
+        String choosenPlayerName = GUIManager.getInstance().getUserChoiceDropDown("ASK FIRST PLAYER, GameView askForFirstPlayer", names);
+        for (int i = 0; i < names.length; i++) {
+            if (choosenPlayerName.equals(names[i])) return playerControllers[i];
+        }
+        return null;
     }
 
 }
