3ff2bab3dfdf3a68ee3ddd3e2250b62d55974981
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-07 12:57:29 +0100

    PlayerView: Added constructors and movePlayer now changes the players position in the GUI.

--- src/main/java/com/gruppe21/player/PlayerView.java
+++ src/main/java/com/gruppe21/player/PlayerView.java
@@ -2,15 +2,29 @@ package com.gruppe21.player;
 
 import com.gruppe21.gui.GUIManager;
 import com.gruppe21.squares.controllers.SquareController;
+import gui_fields.GUI_Car;
+import gui_fields.GUI_Field;
+import gui_fields.GUI_Player;
+
+import java.awt.*;
 
 public class PlayerView {
+    private GUI_Player gui_player;
+
+    public PlayerView(String name, int balance, GUI_Car car){
+        gui_player = new GUI_Player(name, balance, car);
+    }
+
+    public PlayerView(String name, int balance, Color primaryColor, Color secondaryColor, GUI_Car.Type carType, GUI_Car.Pattern pattern){
+        this(name, balance, new GUI_Car(primaryColor, secondaryColor, carType, pattern));
+    }
 
     public void rollDice(int... diceValues){
         GUIManager.getInstance().waitForUserAcknowledgement("ROLL TEXT (PlayerView rollDice)"); //TODO: use localisation
     }
 
     public void movePlayer(SquareController squareController){
-
+        GUIManager.getInstance().setPlayerPosition(gui_player, squareController.getSquareField());
     }
 
 
