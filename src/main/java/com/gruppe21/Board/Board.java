50efed2383dfb4396fab2646a191fcdc0c60320e
Troels <troelstoy@outlook.com>
2021-01-07 16:19:12 +0100

    Added board constructor

--- src/main/java/com/gruppe21/Board/Board.java
+++ src/main/java/com/gruppe21/Board/Board.java
@@ -1,4 +1,12 @@
 package com.gruppe21.Board;
 
+import com.gruppe21.squares.models.Square;
+
 public class Board {
+
+    Square square;
+
+    public Board(Square square){
+        this.square = square;
+    }
 }
