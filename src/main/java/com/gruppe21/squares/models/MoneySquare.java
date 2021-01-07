6c9f56023f9e851718c6530a6230d18d10b0ad3c
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 14:03:26 +0100

    MoneySquare: guiField currently set to only a GUI_Start object

--- src/main/java/com/gruppe21/squares/models/MoneySquare.java
+++ src/main/java/com/gruppe21/squares/models/MoneySquare.java
@@ -1,7 +1,10 @@
 package com.gruppe21.squares.models;
 
+import gui_fields.GUI_Start;
+
 public class MoneySquare extends Square {
     public MoneySquare(int statusEffect, int id) {
         super(statusEffect, id);
+        setGuiField(new GUI_Start());
     }
 }
