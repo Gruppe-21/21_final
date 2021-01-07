5efa975b8eda16bd272332ae3494e07282b27a6a
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 14:02:42 +0100

    TeleportSquare: Set the guifield to a GUI_Empty object

--- src/main/java/com/gruppe21/squares/models/TeleportSquare.java
+++ src/main/java/com/gruppe21/squares/models/TeleportSquare.java
@@ -1,7 +1,11 @@
 package com.gruppe21.squares.models;
 
+import gui_fields.GUI_Empty;
+import gui_fields.GUI_Jail;
+
 public class TeleportSquare extends Square {
     public TeleportSquare(int statusEffect, int id) {
         super(statusEffect, id);
+        setGuiField(new GUI_Empty());
     }
 }
