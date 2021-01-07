5cc5df5a80447072e71a82ac9662d615864e0ffd
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-07 15:57:44 +0100

    GameView: added selectLanguage and askNumberOfPlayers

--- src/main/java/com/gruppe21/game/GameView.java
+++ src/main/java/com/gruppe21/game/GameView.java
@@ -1,4 +1,24 @@
 package com.gruppe21.game;
 
+import com.gruppe21.gui.GUIManager;
+import com.gruppe21.utils.localisation.Localisation;
+
 public class GameView {
+
+    public GameView(){
+    }
+
+    public void selectLanguage(){
+        Localisation.getInstance().setCurrentLocale(
+                GUIManager.getInstance().getUserChoiceDropDown(
+                        "SELECT LANGUAGE, GameView selectLanguage",
+                        Localisation.getInstance().getAllLocales()
+                )
+        );
+    }
+
+    public int askNumberOfPlayers(int minNumPlayers, int maxNumPlayers){
+        return GUIManager.getInstance().getUserInteger("ASK NUM PLAYERS, GameView askNumberOfPlayers", minNumPlayers, maxNumPlayers);
+    }
+
 }
