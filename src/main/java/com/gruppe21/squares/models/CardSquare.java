003bbce6f991b8b6ec08263c2e8e591f7924b95f
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 12:58:58 +0100

    CardSquare: Add GUI_Chance

--- src/main/java/com/gruppe21/squares/models/CardSquare.java
+++ src/main/java/com/gruppe21/squares/models/CardSquare.java
@@ -1,7 +1,11 @@
 package com.gruppe21.squares.models;
 
+import gui_fields.GUI_Chance;
+
 public class CardSquare extends Square {
     public CardSquare(int statusEffect, int id) {
         super(statusEffect, id);
+        GUI_Chance chance = new GUI_Chance();
+        setGuiField(new GUI_Chance());
     }
 }
