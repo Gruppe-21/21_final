51a905d8947886df2651c0f94f05bb1afc087589
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 14:04:08 +0100

    PropertySquare: Added xml tag based constructor

--- src/main/java/com/gruppe21/squares/models/PropertySquare.java
+++ src/main/java/com/gruppe21/squares/models/PropertySquare.java
@@ -1,16 +1,34 @@
 package com.gruppe21.squares.models;
 
+import com.gruppe21.utils.ColorUtil;
+import gui_fields.GUI_Empty;
 import gui_fields.GUI_Field;
 import gui_fields.GUI_Street;
+import org.w3c.dom.Element;
+
+import java.awt.*;
+
+import static java.lang.Integer.parseInt;
 
 public class PropertySquare extends Square {
     private int houses;
+    private int price;
 
-    public PropertySquare(int statusEffect, int id) {
-        super(statusEffect, id);
+    public PropertySquare(int id, String label, String description, Color color, int statusEffect, int price) {
+        super(id, label, description, color, statusEffect);
+        this.price = price;
         houses = 0;
         GUI_Street street = new GUI_Street();
         setGuiField(street);
+    }
+
+    public PropertySquare(Element xmlTag){
+        this(   parseInt(xmlTag.getAttribute("id")), //Id
+                xmlTag.getAttribute("label"), // Name ID
+                xmlTag.getAttribute("description"), // Description ID
+                ColorUtil.getColor(xmlTag.getAttribute("color")), // Color
+                0, // StatusEffect
+                parseInt(xmlTag.getAttribute("price"))); // price
     }
 
     public int getHouses() {
@@ -19,5 +37,13 @@ public class PropertySquare extends Square {
 
     public void setHouses(int houses) {
         this.houses = houses;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
