8c34a746cee1ce313989380e6fdab0c10546cdd3
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-07 16:00:07 +0100

    GameController: added initPlayers and constructor

--- src/main/java/com/gruppe21/game/GameController.java
+++ src/main/java/com/gruppe21/game/GameController.java
@@ -1,2 +1,29 @@
-package com.gruppe21.game;public class GameController {
+package com.gruppe21.game;
+
+
+import com.gruppe21.player.PlayerController;
+
+public class GameController {
+    private static GameController gameController;
+
+    public static GameController getInstance(){
+        if (gameController == null) gameController = new GameController();
+        return gameController;
+    }
+
+    Game game;
+    GameView gameView;
+    private GameController(){
+        gameView = new GameView();
+        game = new Game(gameView.askNumberOfPlayers(Game.MIN_PLAYERS, Game.MAX_PLAYERS));
+        initPlayers();
+    }
+
+    private void initPlayers(){
+        for (int i = 0; i < game.getNumPlayers(); i++) {
+            game.getPlayerControllers()[i] = new PlayerController();
+        }
+    }
+    
+
 }
