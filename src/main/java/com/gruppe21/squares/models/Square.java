fca99f895a10062c2dd2e59e8288da82e4b75f2b
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 09:44:49 +0100

    added javadocs to Square.java

--- src/main/java/com/gruppe21/squares/models/Square.java
+++ src/main/java/com/gruppe21/squares/models/Square.java
@@ -1,8 +1,19 @@
 package com.gruppe21.squares.models;
 
 public class Square {
+    /**
+     * The effect that this square should apply to player when landed on
+     */
     private int statusEffect;
+    /**
+     * Identifier for the square.
+     * Use this for referencing on the board.
+     */
     private int id;
+
+    /**
+     * Identifier used for getting the localized string
+     */
     private String localisationId;
 
     public Square(int statusEffect, int id) {
@@ -10,27 +21,16 @@ public class Square {
         this.id = id;
     }
 
-    public int getStatusEffect() {
-        return statusEffect;
-    }
+    public int getStatusEffect() { return statusEffect; }
 
-    public void setStatusEffect(int statusEffect) {
-        this.statusEffect = statusEffect;
-    }
+    public void setStatusEffect(int statusEffect) { this.statusEffect = statusEffect; }
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setId(int id) { this.id = id; }
 
-    public String getLocalisationId() {
-        return localisationId;
-    }
+    public String getLocalisationId() { return localisationId; }
 
-    public void setLocalisationId(String localisationId) {
-        this.localisationId = localisationId;
-    }
+    public void setLocalisationId(String localisationId) { this.localisationId = localisationId; }
+
 }
