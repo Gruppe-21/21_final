e3f27017826c52949db23c400b31456a2c8f9dd9
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-07 09:35:39 +0100

    added String localisationId (label) to square with getter and setter

--- src/main/java/com/gruppe21/squares/models/Square.java
+++ src/main/java/com/gruppe21/squares/models/Square.java
@@ -3,6 +3,7 @@ package com.gruppe21.squares.models;
 public class Square {
     private int statusEffect;
     private int id;
+    private String localisationId;
 
     public Square(int statusEffect, int id) {
         this.statusEffect = statusEffect;
@@ -23,5 +24,13 @@ public class Square {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getLocalisationId() {
+        return localisationId;
+    }
+
+    public void setLocalisationId(String localisationId) {
+        this.localisationId = localisationId;
     }
 }
