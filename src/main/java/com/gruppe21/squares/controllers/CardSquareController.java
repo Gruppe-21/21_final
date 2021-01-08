303e7563fc02d80c79a427db18e7fd18418178f6
Troels <s152780@student.dtu.dk>
2021-01-08 14:02:51 +0100

    Tried to fix Deck.nextCard

--- src/main/java/com/gruppe21/squares/controllers/CardSquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/CardSquareController.java
@@ -1,5 +1,6 @@
 package com.gruppe21.squares.controllers;
 
+import com.gruppe21.card.cardControllers.CardController;
 import com.gruppe21.deck.Deck;
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.models.Square;
