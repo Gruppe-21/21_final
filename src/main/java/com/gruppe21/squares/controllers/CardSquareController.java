d7bfdd5778c75c27ed75452415a9ccec2ed30da6
Troels <s152780@student.dtu.dk>
2021-01-08 13:47:55 +0100

    Added some minor changes

--- src/main/java/com/gruppe21/squares/controllers/CardSquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/CardSquareController.java
@@ -1,5 +1,6 @@
 package com.gruppe21.squares.controllers;
 
+import com.gruppe21.deck.Deck;
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.models.Square;
 import com.gruppe21.squares.views.SquareView;
@@ -7,10 +8,12 @@ import com.gruppe21.squares.views.SquareView;
 public class CardSquareController extends SquareController {
     public CardSquareController(Square model, SquareView view) {
         super(model, view);
+
     }
 
     @Override
     public void onMoveTo(PlayerController playerController) {
         super.onMoveTo(playerController);
+        return Deck.nextCard();
     }
 }
