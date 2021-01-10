354144f32b113b50d8a409b3e712b9ad5384b89e
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-10 20:20:00 +0100

    Card: added protected attribute deckToReturnTo and the methods setDeckToReturnTo and getDeckToReturnTo

--- src/main/java/com/gruppe21/card/typeOfCards/Card.java
+++ src/main/java/com/gruppe21/card/typeOfCards/Card.java
@@ -1,8 +1,11 @@
 package com.gruppe21.card.typeOfCards;
 
+import com.gruppe21.deck.Deck;
+
 public abstract class Card {
     protected String descriptionOnDrawLabel;
     protected String descriptionOnUseLabel;
+    protected Deck deckToReturnTo;
 
     public Card(String descriptionOnDrawLabel, String descriptionOnUseLabel) {
         this.descriptionOnDrawLabel = descriptionOnDrawLabel;
@@ -16,4 +19,12 @@ public abstract class Card {
     public String getDescriptionOnUseLabel() {
         return descriptionOnDrawLabel;
     }
+
+    public void setDeckToReturnTo(Deck deckToReturnTo){
+        this.deckToReturnTo = deckToReturnTo;
+    }
+    public Deck getDeckToReturnTo(){
+        return deckToReturnTo;
+    }
+
 }
