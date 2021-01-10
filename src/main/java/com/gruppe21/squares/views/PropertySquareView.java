034eceeda207ef9e85439c5bcd1f2d70ee76d079
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-10 15:23:36 +0100

    PropertySquareView: updateOwner now works if the owner is null. Also the borderColor assignment is slightly different

--- src/main/java/com/gruppe21/squares/views/PropertySquareView.java
+++ src/main/java/com/gruppe21/squares/views/PropertySquareView.java
@@ -12,8 +12,15 @@ public class PropertySquareView extends SquareView{
      *
      * @param model
      */
-    public void updateOwner(PropertySquare model){
-        model.getGuiField().setBorder(borderColor, model.getOwner().getColor());
+    public void updateOwner(PropertySquare model) {
+        if (model.getOwner() == null) {
+            model.getGuiField().setBorder(borderColor);
+        } else {
+            Color[] borderColors = model.getOwner().getColors();
+            if (borderColors[0].equals(borderColors[1])) borderColors[1] = borderColor;
+            model.getGuiField().setBorder(borderColors[0], borderColors[1]);
+            //model.getGuiField().setBorder(borderColors[0], borderColor);
+        }
         updateRent(model);
     }
 
