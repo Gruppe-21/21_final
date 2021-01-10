c0c2b65664e5baa77024c3da6f3019b019f1381d
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-10 13:50:08 +0100

    PropertySquareController: added getOwner and setOwner methods. Constructor now only takes PropertySquareView and not any SquareView.

--- src/main/java/com/gruppe21/squares/controllers/PropertySquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/PropertySquareController.java
@@ -1,16 +1,38 @@
 package com.gruppe21.squares.controllers;
 
 import com.gruppe21.player.PlayerController;
+import com.gruppe21.squares.models.PropertySquare;
 import com.gruppe21.squares.models.Square;
+import com.gruppe21.squares.views.PropertySquareView;
 import com.gruppe21.squares.views.SquareView;
+import gui_fields.GUI_Empty;
+import gui_fields.GUI_Street;
 
 public class PropertySquareController extends SquareController {
-    public PropertySquareController(Square model, SquareView view) {
+    PropertySquare model;
+    PropertySquareView view;
+    public PropertySquareController(PropertySquare model, PropertySquareView view) {
         super(model, view);
     }
 
     @Override
     public void onMoveTo(PlayerController playerController) {
         super.onMoveTo(playerController);
+    }
+
+
+    public PlayerController getOwner(){
+        return model.getOwner();
+    }
+
+    /**
+     * Removes the property from the old owners list of owned properties and adds it to the new owners list and sets the owner to the new owner.
+     * @param newOwner the {@code PlayerController} of the new owner.
+     */
+    public void setOwner(PlayerController newOwner){
+        if (model.getOwner() != null) model.getOwner().removeOwnedProperty(this);
+        if (newOwner != null) newOwner.addOwnedProperty(this);
+        model.setOwner(newOwner);
+
     }
 }
