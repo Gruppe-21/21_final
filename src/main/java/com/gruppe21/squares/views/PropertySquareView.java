534502f84091e632970c48a862bb5258ad8ad4d6
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-10 13:50:45 +0100

    initial commit of PropertySquareView

---
+++ src/main/java/com/gruppe21/squares/views/PropertySquareView.java
@@ -0,0 +1,22 @@
+package com.gruppe21.squares.views;
+
+import com.gruppe21.squares.models.PropertySquare;
+
+public class PropertySquareView extends SquareView{
+
+    /**
+     *
+     * @param model
+     */
+    public void updateOwner(PropertySquare model){
+
+    }
+
+    /**
+     *
+     * @param numHouses
+     */
+    public void setHouses(int numHouses){
+
+    }
+}
