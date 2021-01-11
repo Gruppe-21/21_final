68c3b53ca03eb8c602ed243a919be61f0fe37cbd
Troels <s152780@student.dtu.dk>
2021-01-11 10:57:54 +0100

    Tried to fix GoToPrison()

--- src/main/java/com/gruppe21/prison/Prison.java
+++ src/main/java/com/gruppe21/prison/Prison.java
@@ -1,6 +1,19 @@
 package com.gruppe21.prison;
 
+import com.gruppe21.squares.controllers.SquareController;
+
 public class Prison {
 
+    private boolean isImprisoned;
 
+    public boolean GoToPrison(Playercontroller playercontroller , SquareController squareController, boolean isimprisoned){
+        this.isImprisoned = isimprisoned.isImprisoned(true);
+
+        return isImprisoned;
+
+
+    }
+    public void GetOutOfPrison(){
+        return isImprisoned(false);
+    }
 }
