6455b6308fac538eddf39018efba2cb40c305684
Hildibjørg Didriksen <s164539@student.dtu.dk>
2021-01-11 14:12:57 +0100

    TeleportToNearestCardController are done

---
+++ src/main/java/com/gruppe21/card/cardControllers/TeleportToNearestCardController.java
@@ -0,0 +1,43 @@
+package com.gruppe21.card.cardControllers;
+
+import com.gruppe21.card.CardView;
+import com.gruppe21.card.typeOfCards.MoveRelativeCard;
+import com.gruppe21.card.typeOfCards.TeleportToNearestCard;
+import com.gruppe21.game.Board;
+import com.gruppe21.player.PlayerController;
+
+public class TeleportToNearestCardController extends CardController{
+    private TeleportToNearestCard card;
+    private CardView view;
+
+    public TeleportToNearestCardController(CardView view, TeleportToNearestCard card) {
+        super(view, card);
+    }
+
+
+    /**
+     * Calls card use method when the {code:drawer}
+     * @param drawer Player that draws card
+     */
+    @Override
+    public void onDraw(PlayerController drawer) {
+        use(drawer);
+    }
+
+    /**
+     * Method that shows description and moves player {code user} to specific squareID
+     * @param user Player that uses card
+     */
+    @Override
+    public void use(PlayerController user, Board board){
+        super.use(user);
+        int moveToSquareID = card.getSquareID();
+
+        // move player to prison square ID
+        user.moveTo(board.getSquareControllerFromId(moveToSquareID));
+
+        // return card
+        user.getHeldCards().removeCard(this);
+        getReturnDeck().returnCard(this);
+    }
+}
