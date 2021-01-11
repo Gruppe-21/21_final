7d7c930cb84b090619539a9c9b40838e8d3e2cb5
Ledima-TN <63515130+Ledima-TN@users.noreply.github.com>
2021-01-11 13:26:54 +0100

    Fixed selection of languages

--- src/main/java/com/gruppe21/game/GameView.java
+++ src/main/java/com/gruppe21/game/GameView.java
@@ -14,7 +14,7 @@ public class GameView {
                 GUIManager.getInstance().getUserChoiceDropDown(
                         "SELECT LANGUAGE, GameView selectLanguage",
                         Localisation.getInstance().getAllLocales()
-                )
+                ).substring(0,5)
         );
     }
 
