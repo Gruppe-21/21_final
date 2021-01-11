b2008e22c0c2f7a6b281ff17d4de5015113a5e7f
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-11 12:28:27 +0100

    Board: implemented getSquareControllerRelativeTo and getDistanceBetween methods

--- src/main/java/com/gruppe21/game/Board.java
+++ src/main/java/com/gruppe21/game/Board.java
@@ -35,13 +35,20 @@ public class Board {
         return null;
     }
 
-    public int getDistanceBetween(SquareController a, SquareController b) {
-// USE Math.abs(b - a)
-        return 0;
+    /**
+     * Returns the distance going clockwise around the board between two squares.
+     * @param from
+     * @param to
+     * @return
+     */
+    public int getDistanceBetween(SquareController from, SquareController to) {
+        int a = getIndexOfSquareController(from), b = getIndexOfSquareController(to);
+        if (b < a) return getSquareControllers().length - a + b;
+        else return b - a;
     }
 
     public SquareController getSquareControllerRelativeTo(SquareController squareController, int distance){
-        return null;
+        return getSquareControllers()[(getIndexOfSquareController(squareController) + distance) % getSquareControllers().length];
     }
 
 
@@ -53,7 +60,18 @@ public class Board {
         this.squareControllers = squareControllers;
     }
 
+    /**
+     *
+     * @return
+     */
     public SquareController getFirstSquareController() {
     return squareControllers[0];
+    }
+
+    private int getIndexOfSquareController(SquareController squareController){
+        for (int i = 0; i < getSquareControllers().length; i++) {
+            if (squareControllers[i] == squareController) return i;
+        }
+        return -1;
     }
 }
