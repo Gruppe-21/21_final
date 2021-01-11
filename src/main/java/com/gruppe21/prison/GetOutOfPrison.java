183f4bbfc99793a6b7a337476da6308dff825b78
Troels <s152780@student.dtu.dk>
2021-01-11 13:46:55 +0100

    Added payment to GetOutOfPrison

--- src/main/java/com/gruppe21/prison/GetOutOfPrison.java
+++ src/main/java/com/gruppe21/prison/GetOutOfPrison.java
@@ -1,14 +1,21 @@
 package com.gruppe21.prison;
 
+import com.gruppe21.game.Board;
+import com.gruppe21.player.PlayerController;
 import com.gruppe21.player.StatusEffects;
 
 public class GetOutOfPrison {
 
     private StatusEffects statuseffects;
+    PlayerController playercontroller;
 
-    public StatusEffects GetOutOfPrison(StatusEffects statusEffects){
+    public StatusEffects GetOutOfPrison(PlayerController playerController, StatusEffects statusEffects){
         this.statuseffects = statusEffects;
         statuseffects.setImprisoned(false);
+
+        this.playercontroller = playerController;
+
+        playercontroller.transferMoney(1000, null); // Hvordan fører man penge tilbage til banken?
         return statuseffects;
     }
 }
