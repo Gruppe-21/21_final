9d6b8c9cbf107f3a6bfdf365e1cc85d95136ca18
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-12 11:51:30 +0100

    PropertySquareController: added getTotalNumberOfHouses, getTotalNumberOfHotels and getTotalNumberOfBuildings methods

--- src/main/java/com/gruppe21/player/PlayerController.java
+++ src/main/java/com/gruppe21/player/PlayerController.java
@@ -283,6 +283,41 @@ public class PlayerController {
         return player.getStatusEffects();
     }
 
+    /**
+     * Returns the total number of buildings owned by the player, that is, the sum of all owned hotels and houses.
+     * @return an {@code int} representing the total number of buildings owned by the player
+     */
+    public int getTotalNumberOfBuildings(){
+        return getTotalNumberOfHouses() + getTotalNumberOfHotels();
+    }
+
+    /**
+     * Returns the total number of houses owned by the player.
+     * @return an {@code int} representing the total number of house owned by the player
+     */
+    public int getTotalNumberOfHouses(){
+        int numberOfHouses = 0;
+        for (PropertySquareController property: getPlayer().getBuildableProperties()) {
+            if (property.getNumHouses() != property.getMaxNumHouses()) numberOfHouses += property.getNumHouses();
+        }
+        return numberOfHouses;
+    }
+
+    /**
+     * Returns the total number of hotels owned by the player.
+     * @return an {@code int} representing the total number of hotels owned by the player
+     */
+    public int getTotalNumberOfHotels(){
+        int numberOfHotels = 0;
+        for (PropertySquareController property: getPlayer().getBuildableProperties()) {
+            if (property.getNumHouses() != property.getMaxNumHouses()) numberOfHotels += property.getNumHouses();
+        }
+        return numberOfHotels;
+    }
+
+
+
+
     //This makes me sad
     public int getLastRollForBrewery(){
         return lastRollForBrewery;
