869866a947e9c14d50a63304b434a025aaa068ee
Troels <s152780@student.dtu.dk>
2021-01-12 11:50:38 +0100

    Added OnMoveTo() to MoneySquareController

--- src/main/java/com/gruppe21/squares/controllers/CardSquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/CardSquareController.java
@@ -1,7 +1,5 @@
 package com.gruppe21.squares.controllers;
 
-import com.gruppe21.card.cardControllers.CardController;
-import com.gruppe21.deck.Deck;
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.models.Square;
 import com.gruppe21.squares.views.SquareView;
@@ -12,9 +10,7 @@ public class CardSquareController extends SquareController {
     }
 
     @Override
-    public CardController onMoveTo(PlayerController playerController, Deck deck) {
+    public void onMoveTo(PlayerController playerController) {
         super.onMoveTo(playerController);
-        CardController cardController = deck.nextCard();
-        return cardController;
     }
 }
--- src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
@@ -2,22 +2,31 @@ package com.gruppe21.squares.controllers;
 
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.models.MoneySquare;
+import com.gruppe21.squares.models.Square;
 import com.gruppe21.squares.views.SquareView;
 
 public class MoneySquareController extends SquareController {
 
     MoneySquare model;
     SquareView view;
+    PlayerController PlayerController;
 
-    public MoneySquareController(MoneySquare model, SquareView view){
+    public Test(MoneySquare model, SquareView view){
         super(model, view);
         this.model = model;
         this.view = view;
 
+
+    }
+
+    public MoneySquareController(Square model, SquareView view) {
+        super(model, view);
     }
 
     public void OnMoveTo(PlayerController playercontroller){
+        this.PlayerController = playercontroller;
 
+        PlayerController.
     }
 
 }
