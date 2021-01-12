e77292b87feb805b8e3e0a47777c0185a02b26c9
Troels <s152780@student.dtu.dk>
2021-01-12 14:03:13 +0100

    Trying to fix PrisonID()

--- src/main/java/com/gruppe21/squares/controllers/TeleportSquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/TeleportSquareController.java
@@ -3,6 +3,7 @@ package com.gruppe21.squares.controllers;
 import com.gruppe21.game.Board;
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.player.StatusEffects;
+import com.gruppe21.squares.models.MoneySquare;
 import com.gruppe21.squares.models.Square;
 import com.gruppe21.squares.views.SquareView;
 import org.w3c.dom.Element;
@@ -25,11 +26,10 @@ public class TeleportSquareController extends SquareController {
     public void onMoveTo(PlayerController playercontroller, Board board){
 
         playercontroller.getStatusEffects().setImprisoned(true);
-        playercontroller.teleportTo(board.getSquareControllerFromId(31)); //Skal ændres så der bliver fundet et id ud fra XML filen (ID 31)
+        playercontroller.teleportTo(board.getSquareControllerFromId(PrisonID(MoneySquare))); //Skal ændres så der bliver fundet et id ud fra XML filen (ID 31)
 
     }
 
-}
 
     private int PrisonID(Element tag) {
 
