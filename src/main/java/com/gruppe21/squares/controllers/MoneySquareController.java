13847b758d62ae9576f10620ebe412de035013b3
Troels <s152780@student.dtu.dk>
2021-01-12 11:58:38 +0100

    Added todo

--- src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
@@ -4,6 +4,7 @@ import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.models.MoneySquare;
 import com.gruppe21.squares.models.Square;
 import com.gruppe21.squares.views.SquareView;
+import com.sun.tools.javac.comp.Todo;
 
 public class MoneySquareController extends SquareController {
 
@@ -22,11 +23,14 @@ public class MoneySquareController extends SquareController {
     public MoneySquareController(Square model, SquareView view) {
         super(model, view);
     }
-
+    /*
+    *   Skal laves om til at læse fra XML filen og finde den rigtige værdi der
+    *
+    * */
     public void OnMoveTo(PlayerController playercontroller){
         this.PlayerController = playercontroller;
 
-        PlayerController.transferMoney(-10000, null);
+        PlayerController.transferMoney(-4000, null); //ID 5 (4000) og ID 39 (2000)
     }
 
 }
