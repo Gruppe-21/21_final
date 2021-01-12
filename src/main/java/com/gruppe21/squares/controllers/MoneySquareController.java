a770794a63f53c005d9e560c9274c5e41096d0c7
Troels <s152780@student.dtu.dk>
2021-01-12 11:34:08 +0100

    Added MoneySquareController

--- src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
@@ -1,6 +1,12 @@
 package com.gruppe21.squares.controllers;
 
+import com.gruppe21.player.PlayerController;
+
 public class MoneySquareController {
+
+    public MoneySquareController(PlayerController playerController){
+
+    }
 
 
 }
