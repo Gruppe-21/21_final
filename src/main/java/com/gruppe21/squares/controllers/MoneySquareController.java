f78c905cd549a50915345a72adf3d39aa320c43a
Troels <s152780@student.dtu.dk>
2021-01-12 11:43:28 +0100

    Added OnMoveTo() to MoneySquareController

--- src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
+++ src/main/java/com/gruppe21/squares/controllers/MoneySquareController.java
@@ -1,12 +1,23 @@
 package com.gruppe21.squares.controllers;
 
 import com.gruppe21.player.PlayerController;
+import com.gruppe21.squares.models.MoneySquare;
+import com.gruppe21.squares.views.SquareView;
 
-public class MoneySquareController {
+public class MoneySquareController extends SquareController {
 
-    public MoneySquareController(PlayerController playerController){
+    MoneySquare model;
+    SquareView view;
+
+    public MoneySquareController(MoneySquare model, SquareView view){
+        super(model, view);
+        this.model = model;
+        this.view = view;
 
     }
 
+    public void OnMoveTo(PlayerController playercontroller){
+
+    }
 
 }
