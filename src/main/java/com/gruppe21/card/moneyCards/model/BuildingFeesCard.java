b76b4ec65f29034864b705d5aba4fa0f4dc97367
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 13:21:58 +0100

    BuildingFeesCard: added overload to constructor

--- src/main/java/com/gruppe21/card/moneyCards/model/BuildingFeesCard.java
+++ src/main/java/com/gruppe21/card/moneyCards/model/BuildingFeesCard.java
@@ -1,11 +1,23 @@
 package com.gruppe21.card.moneyCards.model;
 
+import org.w3c.dom.Element;
+
+import static java.lang.Integer.parseInt;
+
 public class BuildingFeesCard extends ModifyMoneyCard {
     private final int hotelFee;
 
     public BuildingFeesCard(String descriptionOnDrawLabel, String descriptionOnUseLabel, int houseFee, int hotelFee) {
         super(descriptionOnDrawLabel, descriptionOnUseLabel, houseFee);
         this.hotelFee = hotelFee;
+    }
+
+    public BuildingFeesCard(Element xmlTag){
+        this(xmlTag.getAttribute("onDrawDescription"),
+                xmlTag.getAttribute("onUseDescription"),
+                parseInt(xmlTag.getAttribute("houseFee")),
+                parseInt(xmlTag.getAttribute("hotelFee"))
+        );
     }
 
     public int getHouseFee(){
