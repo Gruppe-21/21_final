9e502933d47e580b3e7806070397bcc67b8ce59c
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 11:39:24 +0100

    MoveToNearestCardController now inherits from TeleportToNearestCardController

--- src/main/java/com/gruppe21/card/cardControllers/MoveToNearestCardController.java
+++ src/main/java/com/gruppe21/card/cardControllers/MoveToNearestCardController.java
@@ -1,42 +1,20 @@
 package com.gruppe21.card.cardControllers;
 
 import com.gruppe21.card.cardType.MoveToNearestCard;
+import com.gruppe21.card.cardType.TeleportToNearestCard;
 import com.gruppe21.card.cardView.CardView;
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.controllers.SquareController;
 
-public class MoveToNearestCardController extends CardController {
+public class MoveToNearestCardController extends TeleportToNearestCardController {
 
-    public MoveToNearestCardController(CardView cardView, MoveToNearestCard card) {
+    public MoveToNearestCardController(CardView cardView, TeleportToNearestCard card) {
         super(cardView, card);
     }
 
     @Override
-    public void onDraw(PlayerController drawer) {
-        use(drawer);
-    }
-
-    /**
-     * Method finds the nearest SquareController in {code:board} with Board's closestSquareController()
-     * method.The method calcualtes the {code:user} position relative to the closest squareController
-     * and uses PlayerControllers moveTo() method.
-     * @param user Player using the card
-     * @param board Board which the game is played with
-     */
-    @Override
-    public void use(PlayerController user){
-        MoveToNearestCard mnCard = (MoveToNearestCard)card;
-        super.use(user);
-
-        int[] squaresIdArr = mnCard.getIDSquares();
-        SquareController[] squareControllersArr = new SquareController[squaresIdArr.length];
-
-        // move player to nearest SquareController in squareControllersArr
-        user.moveTo(board.closestSquareController(user.getPlayer().getPosition(), squareControllersArr));
-
-        // return card
-        user.getHeldCards().removeCard(this);
-        getReturnDeck().returnCard(this);
+    protected void relocate(PlayerController user, SquareController destination){
+        user.moveTo(destination);
     }
 
 }
