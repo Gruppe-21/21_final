fc9bd49685b3af65d76d3d9c2436181e902ecfbb
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 13:25:51 +0100

    ModifyMoneyCard: added overload to constructor

--- src/main/java/com/gruppe21/card/moneyCards/model/ModifyMoneyCard.java
+++ src/main/java/com/gruppe21/card/moneyCards/model/ModifyMoneyCard.java
@@ -1,6 +1,9 @@
 package com.gruppe21.card.moneyCards.model;
 
 import com.gruppe21.card.model.Card;
+import org.w3c.dom.Element;
+
+import static java.lang.Integer.parseInt;
 
 public class ModifyMoneyCard extends Card {
     private final int modifyValue;
@@ -8,6 +11,13 @@ public class ModifyMoneyCard extends Card {
     public ModifyMoneyCard(String descriptionOnDrawLabel, String descriptionOnUseLabel, int modifyValue) {
         super(descriptionOnDrawLabel, descriptionOnUseLabel);
         this.modifyValue = modifyValue;
+    }
+
+    public ModifyMoneyCard(Element xmlTag){
+        this(xmlTag.getAttribute("onDrawDescription"),
+                xmlTag.getAttribute("onUseDescription"),
+                parseInt(xmlTag.getAttribute("modifyValue"))
+        );
     }
 
 
