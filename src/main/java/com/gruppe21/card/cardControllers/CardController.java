a165a28b6b4f94b7af152db86deae036788c477d
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 10:29:20 +0100

    CardController: removed overloaded use function

--- src/main/java/com/gruppe21/card/cardControllers/CardController.java
+++ src/main/java/com/gruppe21/card/cardControllers/CardController.java
@@ -3,7 +3,6 @@ package com.gruppe21.card.cardControllers;
 import com.gruppe21.card.cardView.CardView;
 import com.gruppe21.card.cardType.Card;
 import com.gruppe21.deck.Deck;
-import com.gruppe21.game.Board;
 import com.gruppe21.player.PlayerController;
 
 
@@ -35,14 +34,9 @@ public abstract class CardController {
      * @param user Player that uses card
      */
     public void use(PlayerController user){
-        if (card == null || cardView == null) return; //This should never happen
         cardView.displayCard(card.getDescriptionOnUseLabel());
     }
 
-    // Used for cards with connection to board.
-    public void use(PlayerController user, Board board){
-        cardView.displayCard(card.getDescriptionOnUseLabel());
-    }
 
     /**
      * Returns the {@code Class} object of the {@code CardController}'s {@code Card}
