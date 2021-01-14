f53cb32902a8eae6a3f2fb5bab5139d97a73c604
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 12:13:36 +0100

    Added RelocationCardController class which inherits from UseOnDrawCardController

---
+++ src/main/java/com/gruppe21/card/cardControllers/RelocationCardController.java
@@ -0,0 +1,35 @@
+package com.gruppe21.card.cardControllers;
+
+import com.gruppe21.card.cardType.Card;
+import com.gruppe21.card.cardView.CardView;
+import com.gruppe21.game.Board;
+import com.gruppe21.player.PlayerController;
+import com.gruppe21.squares.controllers.SquareController;
+
+public abstract class RelocationCardController extends UseOnDrawCardController{
+
+    public RelocationCardController(CardView cardView, Card card){
+        super(cardView, card);
+    }
+
+    /**
+     * Method that shows description and moves player {code user} to specific squareID
+     *
+     * @param user Player that uses card
+     */
+    @Override
+    public void use(PlayerController user, Board board) {
+        super.use(user);
+
+        relocate(user, getDestination(user, board));
+
+        // return card
+        returnToDeck(user);
+    }
+
+    protected abstract void relocate(PlayerController user, SquareController destination);
+
+   protected abstract SquareController getDestination(PlayerController user, Board board);
+
+
+}
