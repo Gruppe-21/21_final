5b30e0eb9970ddc3d0efca25ce4371b3e70a8e57
Tobias Maneschijn <tfrom222@gmail.com>
2021-01-14 11:09:29 +0100

    added test methods to BoardTest

---
+++ src/test/java/com/gruppe21/board/BoardTest.java
@@ -0,0 +1,45 @@
+package com.gruppe21.board;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BoardTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSquareControllerFromId() {
+    }
+
+    @Test
+    void closestSquareController() {
+    }
+
+    @Test
+    void getDistanceBetween() {
+    }
+
+    @Test
+    void getSquareControllerRelativeTo() {
+    }
+
+    @Test
+    void getSquareControllers() {
+    }
+
+    @Test
+    void setSquareControllers() {
+    }
+
+    @Test
+    void getFirstSquareController() {
+    }
+}
