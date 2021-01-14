0c888d078c60184413c3af1ed257a75bdd2ca17b
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 14:43:29 +0100

    PardonCard: added overload to constructor

--- src/main/java/com/gruppe21/card/model/PardonCard.java
+++ src/main/java/com/gruppe21/card/model/PardonCard.java
@@ -1,10 +1,19 @@
 package com.gruppe21.card.model;
 
+import org.w3c.dom.Element;
+
+
 public class PardonCard extends Card{
     protected final String descriptionOnDrawLabel;
     public PardonCard(String descriptionOnDrawLabel, String descriptionOnUseLabel) {
         super(descriptionOnUseLabel);
         this.descriptionOnDrawLabel = descriptionOnDrawLabel;
+    }
+
+    public PardonCard(Element xmlTag){
+        this(xmlTag.getAttribute("onDrawDescription"),
+                xmlTag.getAttribute("onUseDescription")
+        );
     }
 
     public String getDescriptionOnDrawLabel() {
