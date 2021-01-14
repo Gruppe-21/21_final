915f2bd4c287edf3b8f848d6a2de6f7c22a7b304
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 11:12:36 +0100

    MoveToNearestCard: added constructor that takes xml tag

--- src/main/java/com/gruppe21/card/cardType/MoveToNearestCard.java
+++ src/main/java/com/gruppe21/card/cardType/MoveToNearestCard.java
@@ -1,13 +1,29 @@
 package com.gruppe21.card.cardType;
 
-import com.gruppe21.squares.controllers.SquareController;
+import org.w3c.dom.Element;
+import static java.lang.Integer.parseInt;
 
 public class MoveToNearestCard extends Card{
     int[] IDSquares;
 
-    public MoveToNearestCard(String descriptionOnDrawLabel, String descriptionOnUseLabel,int[] IDSquares) {
+    public MoveToNearestCard(String descriptionOnDrawLabel, String descriptionOnUseLabel, int... IDSquares) {
         super(descriptionOnDrawLabel, descriptionOnUseLabel);
         this.IDSquares = IDSquares;
+    }
+
+    public MoveToNearestCard(Element xmlTag){
+        this(xmlTag.getAttribute("onDrawDescription"),
+                xmlTag.getAttribute("onUseDescription"),
+                readIds(xmlTag)
+                );
+    }
+
+    private static int[] readIds(Element xmlTag) {
+        String[] idStr = xmlTag.getAttribute("squareID").split(" ");
+        int[] ids = new int[idStr.length];
+        for (int i = 0; i < ids.length; i++) {
+            ids[i] = parseInt(idStr[i]);
+        }
     }
 
     public int[] getIDSquares() {
