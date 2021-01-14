8bebaafffd9b67124ac7a2a8c5054b41f97b2c87
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 12:15:18 +0100

    MoveToRelativeCardController now inherits from RelocationCardController and added getDestination and relocate methods

--- src/main/java/com/gruppe21/card/cardControllers/MoveToRelativeCardController.java
+++ src/main/java/com/gruppe21/card/cardControllers/MoveToRelativeCardController.java
@@ -1,35 +1,25 @@
 package com.gruppe21.card.cardControllers;
 
+
 import com.gruppe21.card.cardType.MoveRelativeCard;
+import com.gruppe21.card.cardType.TeleportToNearestCard;
 import com.gruppe21.card.cardView.CardView;
 import com.gruppe21.game.Board;
 import com.gruppe21.player.PlayerController;
+import com.gruppe21.squares.controllers.SquareController;
 
-public class MoveToRelativeCardController extends CardController {
-    MoveRelativeCard card;
-    CardView view;
+public class MoveToRelativeCardController extends RelocationCardController {
 
     public MoveToRelativeCardController(CardView cardView, MoveRelativeCard card) {
         super(cardView, card);
     }
 
-
-    @Override
-    public void onDraw(PlayerController drawer) {
-        use(drawer);
+    protected SquareController getDestination(PlayerController user, Board board) {
+        return board.getSquareControllerRelativeTo(user.getPlayer().getPosition(), ((MoveRelativeCard) card).getMoveSquares());
     }
 
-    @Override
-    public void use(PlayerController user, Board board){
-        super.use(user);
-        int moveSquare = card.getMoveSquares();
-
-        // move player a moveSquare distance forwards or backwards
-        user.moveTo(board.getSquareControllerRelativeTo(user.getPlayer().getPosition(),moveSquare));
-
-        // return card
-        user.getHeldCards().removeCard(this);
-        getReturnDeck().returnCard(this);
+    protected void relocate(PlayerController user, SquareController destination){
+        user.moveTo(destination);
     }
 
 }
