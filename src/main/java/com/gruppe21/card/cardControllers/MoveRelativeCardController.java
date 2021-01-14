c85ff18cb7594cd96d4f8e300287a01d1e86a004
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 12:16:13 +0100

    MoveToRelativeCardController renamed to MoveRelativeCardController

--- src/main/java/com/gruppe21/card/cardControllers/MoveRelativeCardController.java
+++ src/main/java/com/gruppe21/card/cardControllers/MoveRelativeCardController.java
@@ -2,15 +2,14 @@ package com.gruppe21.card.cardControllers;
 
 
 import com.gruppe21.card.cardType.MoveRelativeCard;
-import com.gruppe21.card.cardType.TeleportToNearestCard;
 import com.gruppe21.card.cardView.CardView;
 import com.gruppe21.game.Board;
 import com.gruppe21.player.PlayerController;
 import com.gruppe21.squares.controllers.SquareController;
 
-public class MoveToRelativeCardController extends RelocationCardController {
+public class MoveRelativeCardController extends RelocationCardController {
 
-    public MoveToRelativeCardController(CardView cardView, MoveRelativeCard card) {
+    public MoveRelativeCardController(CardView cardView, MoveRelativeCard card) {
         super(cardView, card);
     }
 
