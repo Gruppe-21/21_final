a6b11dfd2b2dbf2a60805a73fd71719717b19929
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-01-14 11:33:13 +0100

    TeleportToNearestCard: added possibleDestinations attribute and getPossibleDestinations setPossibleDestinations

--- src/main/java/com/gruppe21/card/cardType/TeleportToNearestCard.java
+++ src/main/java/com/gruppe21/card/cardType/TeleportToNearestCard.java
@@ -1,15 +1,25 @@
 package com.gruppe21.card.cardType;
 
+import com.gruppe21.squares.controllers.SquareController;
+
 public class TeleportToNearestCard extends Card {
-    private int squareID; //squareIndex prison?
+    private int squareIDs; //squareIndex prison?
+    SquareController[] possibleDestinations;
 
-    public TeleportToNearestCard(String descriptionOnDrawLabel, String descriptionOnUseLabel,int squareID) {
+    public TeleportToNearestCard(String descriptionOnDrawLabel, String descriptionOnUseLabel,int squareIDs) {
         super(descriptionOnDrawLabel, descriptionOnUseLabel);
-        this.squareID = squareID;
+        this.squareIDs = squareIDs;
     }
 
-    public int getSquareID() {
-        return squareID;
+    public int getSquareIDs() {
+        return squareIDs;
     }
 
+    public SquareController[] getPossibleDestinations(){
+        return possibleDestinations
+    }
+
+    public void setPossibleDestinations(SquareController[] possibleDestinations) {
+        this.possibleDestinations = possibleDestinations;
+    }
 }
